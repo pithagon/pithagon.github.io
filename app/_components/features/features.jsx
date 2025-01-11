cca003f2c39ec079d2550b42416f774edc03e8d3
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-11 21:33:03 +0700

    update

--- app/_components/features/features.jsx
+++ app/_components/features/features.jsx
@@ -2,7 +2,7 @@ import { ArrowRightIcon } from '@/components/icons'
 import cn from 'clsx'
 import Link from 'next/link'
 import { MotionDiv } from '@/components/motion'
-import styles from './features.module.css'
+// import styles from './features.module.css'
 
 export function Feature({
     large,
@@ -14,36 +14,38 @@ export function Feature({
     index,
     ...props
 }) {
-    return (
-        <MotionDiv
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true, margin: '-20px' }}
-            transition={{ duration: Math.min(0.25 + index * 0.2, 0.8) }}
-            className={cn(
-                styles.feature,
-                large && styles.large,
-                centered && styles.centered,
-                lightOnly && styles['light-only'],
-                className
-            )}
-            {...props}
-        >
-            {children}
-            {href && (
-                <Link
-                    className={cn('x:focus-visible:nextra-focus', styles.link)}
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <ArrowRightIcon height="24" />
-                </Link>
-            )}
-        </MotionDiv>
-    )
+    // return (
+    //     <MotionDiv
+    //         initial={{ opacity: 0 }}
+    //         whileInView={{ opacity: 1 }}
+    //         viewport={{ once: true, margin: '-20px' }}
+    //         transition={{ duration: Math.min(0.25 + index * 0.2, 0.8) }}
+    //         className={cn(
+    //             styles.feature,
+    //             large && styles.large,
+    //             centered && styles.centered,
+    //             lightOnly && styles['light-only'],
+    //             className
+    //         )}
+    //         {...props}
+    //     >
+    //         {children}
+    //         {href && (
+    //             <Link
+    //                 className={cn('x:focus-visible:nextra-focus', styles.link)}
+    //                 href={href}
+    //                 target="_blank"
+    //                 rel="noreferrer"
+    //             >
+    //                 <ArrowRightIcon height="24" />
+    //             </Link>
+    //         )}
+    //     </MotionDiv>
+    // )
+    return <div>{children}</div>
 }
 
 export function Features({ children }) {
-    return <div className={styles.features}>{children}</div>
+    // return <div className={styles.features}>{children}</div>
+    return <div>{children}</div>
 }
